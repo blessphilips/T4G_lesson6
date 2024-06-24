0c607f1d33c7255560f334aebd14924d15d7d52e
unknown <blessphilips>
2024-06-24 07:30:01 -0700

    Created an Introduction to JS folder

---
+++ src/index.js
@@ -0,0 +1,22 @@
+//  
+
+// let name1 = prompt("what is your name?");
+// alert(name1);
+// alert("Hi " + name1 + ", welcome to our website" );
+// let age = 18;
+// let hometown =Keta;
+// alert("Hi ${name1}, from ${homework}, who is, ${age)");
+
+
+// let title = prompt("What is your title?");
+// let name2 = prompt("What is your name?");
+// let favouriteproduct = prompt("What is your product?");
+// alert(`Hi ${title}, ${name2}, ${favouriteproduct}`);
+
+let name = "Bless";
+alert(name1)
+name = "Vicky joy";
+name = "Jacklin";
+
+
+
