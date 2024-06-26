a689cdff680b4b54f7e87c99b855fa0d136a356f
unknown <blessphilips>
2024-06-26 05:41:44 -0700

    modified src/index.js

--- src/index.js
+++ src/index.js
@@ -8,15 +8,30 @@
 // alert("Hi ${name1}, from ${homework}, who is, ${age)");
 
 
-let title = prompt("What is your title?");
-let name2 = prompt("What is your name?");
-let favouriteproduct = prompt("What is your product?");
-alert(`Hi ${title} ${name2},  is your favorite product ${favouriteproduct}?`);
+// let title = prompt("What is your title?");
+// let name2 = prompt("What is your name?");
+// let favouriteproduct = prompt("What is your product?");
+// alert(`Hi ${title} ${name2},  is your favorite product ${favouriteproduct}?`);
 
-let name = "Bless";
-alert(name1)
-name = "Vicky joy";
-name = "Jacklin";
+// let name = "Bless";
+// alert(name1)
+// name = "Vicky joy";
+// name = "Jacklin";
 
+let name = prompt("What is your name?");
+let age = prompt("How old are you?");
+let email = prompt("Enter your email.");
+alert("Hi ${name}, you are ${age} years old, and we will get keep in touch via your email 👋👋.");
 
+// let name1 = "my name is Bless"; // example of concatination
+// let sentence = "hi there " + name1 ; // example of interpulation
+
+// let sentence1 = `hello 🙋‍♀️ ${name1}`;
+// console.log(sentence);
+// console.log(sentence1);
+
+// let smoothy1 = "milk shake is a mixture of mangoe,bananas and milk";
+// let smoothy = `hi there, 👋 ${smoothy1}`;
+// console.log(smoothy);
+// console.log(smoothy1);
 
